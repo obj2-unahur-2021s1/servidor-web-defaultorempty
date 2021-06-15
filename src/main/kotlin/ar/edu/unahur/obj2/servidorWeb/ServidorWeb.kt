18d8dea8e91d46a686c1bb9b88eba7c9fbd52527
Matias Gonzalez <matias.gonzalez@live.com.ar>
2021-06-14 21:42:43 -0300

    Punto numero 1

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -1,14 +1,34 @@
 package ar.edu.unahur.obj2.servidorWeb
 
+import com.sun.org.apache.xpath.internal.operations.Bool
+import java.net.URI
+import java.net.URL
 import java.time.LocalDateTime
 
 // Para no tener los códigos "tirados por ahí", usamos un enum que le da el nombre que corresponde a cada código
 // La idea de las clases enumeradas es usar directamente sus objetos: CodigoHTTP.OK, CodigoHTTP.NOT_IMPLEMENTED, etc
 enum class CodigoHttp(val codigo: Int) {
-  OK(200),
-  NOT_IMPLEMENTED(501),
-  NOT_FOUND(404),
+    OK(200),
+    NOT_IMPLEMENTED(501),
+    NOT_FOUND(404),
 }
 
 class Pedido(val ip: String, val url: String, val fechaHora: LocalDateTime)
 class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido)
+
+
+class WebServer() {
+
+    fun procesarPeticionSinModulos(pedido: Pedido): Respuesta {
+        return if (evaluarProtocolo(pedido))
+            return Respuesta(CodigoHttp.OK, "OK", 22, pedido)
+        else
+            return Respuesta(CodigoHttp.NOT_IMPLEMENTED, "", 10, pedido)
+    }
+
+    fun evaluarProtocolo(pedido: Pedido): Boolean {
+        return URL(pedido.url).protocol == "http"
+    }
+
+}
+
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -3,8 +3,22 @@ package ar.edu.unahur.obj2.servidorWeb
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import java.time.LocalDateTime
+import java.time.LocalTime
 
 class ServidorWebTest : DescribeSpec({
-  describe("Un servidor web") {
-  }
+    describe("Un servidor web") {
+        var servidor = WebServer()
+
+        it("Codigo 200 - Ok ") {
+            var pedido = Pedido("192.168.1.1", "http://pepito.com.ar/documentos/doc1.html", LocalDateTime.now())
+
+            servidor.procesarPeticionSinModulos(pedido).codigo.shouldBe(CodigoHttp.OK)
+        }
+
+        it("Codigo 501 - Not Implemente ") {
+            var pedido = Pedido("192.168.1.1", "https://pepito.com.ar/documentos/doc1.html", LocalDateTime.now())
+
+            servidor.procesarPeticionSinModulos(pedido).codigo.shouldBe(CodigoHttp.NOT_IMPLEMENTED)
+        }
+    }
 })
