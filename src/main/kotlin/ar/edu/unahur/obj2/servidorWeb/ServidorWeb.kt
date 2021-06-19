646111d6c4dcade9a11af58b494987cafe15da76
sdelafuente <coalsan@gmail.com>
2021-06-18 21:53:46 -0300

    implementación modulos. Tests.

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -1,6 +1,6 @@
 package ar.edu.unahur.obj2.servidorWeb
 
-import com.sun.org.apache.xpath.internal.operations.Bool
+// import com.sun.org.apache.xpath.internal.operations.Bool
 import java.net.URI
 import java.net.URL
 import java.time.LocalDateTime
@@ -19,16 +19,72 @@ class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val p
 
 class WebServer() {
 
-    fun procesarPeticionSinModulos(pedido: Pedido): Respuesta {
-        return if (evaluarProtocolo(pedido))
-            return Respuesta(CodigoHttp.OK, "OK", 22, pedido)
+    var modulos = mutableListOf<Modulo>()
+
+    fun procesarPeticion(pedido: Pedido): Respuesta {
+
+        if (evaluarProtocolo(pedido))
+            return procesarPeticionConModulo(pedido)
         else
             return Respuesta(CodigoHttp.NOT_IMPLEMENTED, "", 10, pedido)
+    }
+
+    fun procesarPeticionSinModulos(pedido: Pedido): Respuesta {
+        return if (evaluarProtocolo(pedido))
+            Respuesta(CodigoHttp.OK, "OK", 22, pedido)
+        else
+            Respuesta(CodigoHttp.NOT_IMPLEMENTED, "", 10, pedido)
+    }
+
+    fun obtenerModuloDeProcesamiento(extension: String): Modulo {
+        return modulos.first { it.extensiones.contains(extension) }
+    }
+
+    fun existeModulo(extension: String): Boolean {
+        return modulos.any { it.extensiones.contains(extension) }
+    }
+
+    fun procesarPeticionConModulo(pedido: Pedido): Respuesta {
+        return if (existeModulo(evaluarExtension(pedido)))
+            obtenerModuloDeProcesamiento(evaluarExtension(pedido)).procesarPedido(pedido)
+        else
+            Respuesta(CodigoHttp.NOT_FOUND, "", 10, pedido)
     }
 
     fun evaluarProtocolo(pedido: Pedido): Boolean {
         return URL(pedido.url).protocol == "http"
     }
 
+    fun evaluarExtension(pedido: Pedido): String {
+        return URL(pedido.url).path.substringAfter('.')
+    }
+
+    fun evaluarRuta(pedido: Pedido): String {
+        return URL(pedido.url).path
+    }
 }
 
+abstract class Modulo {
+    var extensiones = mutableListOf<String>()
+
+    abstract fun procesarPedido(pedido: Pedido): Respuesta
+}
+
+class ModuloTexto() : Modulo() {
+    override fun procesarPedido(pedido: Pedido): Respuesta {
+        return Respuesta(CodigoHttp.OK, "Yo respondo texto", 55, pedido)
+    }
+
+}
+
+class ModuloImagen() : Modulo() {
+    override fun procesarPedido(pedido: Pedido): Respuesta {
+        return Respuesta(CodigoHttp.OK, "Yo respondo Imagen", 65, pedido)
+    }
+}
+
+class ModuloVideo() : Modulo() {
+    override fun procesarPedido(pedido: Pedido): Respuesta {
+        return Respuesta(CodigoHttp.OK, "Yo respondo Video", 75, pedido)
+    }
+}
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -4,6 +4,7 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import java.time.LocalDateTime
 import java.time.LocalTime
+import java.net.URL
 
 class ServidorWebTest : DescribeSpec({
     describe("Un servidor web") {
@@ -19,6 +20,52 @@ class ServidorWebTest : DescribeSpec({
             var pedido = Pedido("192.168.1.1", "https://pepito.com.ar/documentos/doc1.html", LocalDateTime.now())
 
             servidor.procesarPeticionSinModulos(pedido).codigo.shouldBe(CodigoHttp.NOT_IMPLEMENTED)
+        }
+    }
+
+    describe("probar URL") {
+        it("Probar HTTP") {
+            URL("http://pepito.com.ar/documentos/doc1.html").protocol.shouldBe("http")
+        }
+
+        it("Probar file") {
+            URL("http://pepito.com.ar/documentos/doc1.html").file.shouldBe("/documentos/doc1.html")
+        }
+
+        it("Probar file subtring after") {
+            URL("http://pepito.com.ar/documentos/doc1.html").file.substringAfter('.').shouldBe("html")
+        }
+
+        it("Probar host") {
+            URL("http://pepito.com.ar/documentos/doc1.html").host.shouldBe("pepito.com.ar")
+        }
+    }
+
+    describe("Servidor Web") {
+        var servidor = WebServer()
+        var moduloImagenes = ModuloImagen()
+
+        moduloImagenes.extensiones.add("jpg")
+        moduloImagenes.extensiones.add("png")
+        moduloImagenes.extensiones.add("gif")
+
+        servidor.modulos.add(moduloImagenes)
+
+        describe("probar peticion") {
+            it("http invalida") {
+                var pedido = Pedido("120.234.56.678", "https://pepito.com.ar/documentos/doc1.html", LocalDateTime.now())
+                servidor.procesarPeticion(pedido).codigo.shouldBe(CodigoHttp.NOT_IMPLEMENTED)
+            }
+
+            it("formato no valido") {
+                var pedido = Pedido("120.234.56.678", "http://pepito.com.ar/documentos/doc1.html", LocalDateTime.now())
+                servidor.procesarPeticion(pedido).codigo.shouldBe(CodigoHttp.NOT_FOUND)
+            }
+
+            it("formato valido") {
+                var pedido = Pedido("120.234.56.678", "http://pepito.com.ar/documentos/doc1.png", LocalDateTime.now())
+                servidor.procesarPeticion(pedido).codigo.shouldBe(CodigoHttp.OK)
+            }
         }
     }
 })
