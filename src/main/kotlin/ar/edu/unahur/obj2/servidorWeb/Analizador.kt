77c43fde5a03a6bf07bde596dd24672386d89577
Matias Gonzalez <matias.gonzalez@live.com.ar>
2021-06-19 20:50:46 -0300

    commit continuacion analizadores

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/Analizador.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/Analizador.kt
@@ -1,5 +1,6 @@
 package ar.edu.unahur.obj2.servidorWeb
 
+import java.time.LocalDateTime
 import java.util.*
 
 interface Analizador {
@@ -40,5 +41,30 @@ class AnalizadorIpsSospechosa() : Analizador {
 
     fun conjuntoDeIpsPorRuta(ruta: String): MutableList<String> {
         return mapeado.filter { it.key.pedido.url.contains(ruta) }.keys.map { it.pedido.ip }.toMutableList()
+    }
+}
+
+
+class AnalizadorEstadisticas() : Analizador {
+    var listaDeRespuestas = mutableListOf<Respuesta>()
+
+    override fun procesar(respuesta: Respuesta, modulo: Modulo) {
+        listaDeRespuestas.add(respuesta)
+    }
+
+    fun tiempoDeRespuestaPromedio(): Double {
+        return listaDeRespuestas.map { it.tiempo }.average()
+    }
+
+    fun cantidadDePedidosEntreDosMomentos(inicio: LocalDateTime, fin: LocalDateTime): Int {
+        return listaDeRespuestas.filter { it.pedido.fechaHora in inicio..fin }.count()
+    }
+
+    fun cantidadDeRespuestasPorStringEnBody(texto: String): Int {
+        return listaDeRespuestas.filter { it.body.contains(texto) }.count()
+    }
+
+    fun porcentajeDePedidosConRespuestaExitoso(): Int {
+        return listaDeRespuestas.filter { it.codigo == CodigoHttp.OK }.count() / listaDeRespuestas.count() * 100
     }
 }
