fb6a0206dfa0bb748e0e352bb0c8debad32b5eb3
sdelafuente <coalsan@gmail.com>
2021-06-19 20:45:53 -0300

    =fix. Analizador de IPs.

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/Analizador.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/Analizador.kt
@@ -23,30 +23,22 @@ class AnalizadorDeteccionDeDemora(var demoraMinima: Int) : Analizador {
 class AnalizadorIpsSospechosa() : Analizador {
 
     var ipsSospechosas = mutableListOf<String>()
-    var listaDePedidosContenidosEnElRequest = mutableListOf<Respuesta>()
     var mapeado = Hashtable<Respuesta, Modulo>()
 
     override fun procesar(respuesta: Respuesta, modulo: Modulo) {
         if (ipsSospechosas.contains(respuesta.pedido.ip))
             mapeado.put(respuesta, modulo)
-        // listaDePedidosContenidosEnElRequest.add(respuesta)
     }
 
     fun cantidadDePedidosPorIp(ip: String): Int {
-        //  return listaDePedidosContenidosEnElRequest.filter { it.pedido.ip == ip }.count()
         return mapeado.filter { it.key.pedido.ip == ip }.count()
     }
 
     fun moduloMasConsultado(): Modulo? {
-        // listaDePedidosContenidosEnElRequest.maxByOrNull { it.pedido.url }
         return mapeado.maxBy { it.key.pedido.url }?.value
     }
 
-    //fun conjuntoDeIpsPorRuta(ruta : String) = listaDePedidosContenidosEnElRequest.filter { it.pedido.url.contains(ruta) }
     fun conjuntoDeIpsPorRuta(ruta: String): MutableList<String> {
         return mapeado.filter { it.key.pedido.url.contains(ruta) }.keys.map { it.pedido.ip }.toMutableList()
     }
-
-    fun agregarIPSospechosa(ip: String) = ipsSospechosas.add(ip)
-    fun eliminarIPSospechosas(ip: String) = ipsSospechosas.remove(ip)
 }
